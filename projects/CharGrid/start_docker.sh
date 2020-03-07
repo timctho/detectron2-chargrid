3f154e318cd7c95e9b986484c0c30aa2da4d69cb
Tim Ho <timho0110@gmail.com>
2020-03-07 18:13:23 +0800

    add docker starter

---
+++ projects/CharGrid/start_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGE="docker_detectron2:latest"
+
+xhost local:root
+docker run -it \
+           --gpus all \
+           --privileged \
+           -v $(pwd)/../../:/detectron2 \
+           -v /home/timho/data:/data \
+           -w /detectron2/projects/CharGrid \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -e DISPLAY=$(echo $DISPLAY) \
+           -e QT_X11_NO_MITSHM=1 \
+           -e PYTHONPATH=/detectron2/projects/CharGrid \
+           ${IMAGE} \
+           /bin/bash
