5b589c32ea473298ab5046f0b81213fba029358f
Tim Ho <timho0110@gmail.com>
2020-03-14 14:20:29 +0800

    pass video device

--- projects/CharGrid/start_docker.sh
+++ projects/CharGrid/start_docker.sh
@@ -10,6 +10,7 @@ docker run -it \
            -v /home/timho/data:/data \
            -w /detectron2/projects/CharGrid \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v /dev/video0:/dev/video0 \
            -e DISPLAY=$(echo $DISPLAY) \
            -e QT_X11_NO_MITSHM=1 \
            -e PYTHONPATH=/detectron2/projects/CharGrid \
