a909067f1e692f7846c6914f57efcbb0434219fb
Tim Ho <timho0110@gmail.com>
2020-03-15 19:39:32 +0800

    Add example viewer

---
+++ projects/CharGrid/data/view_example.py
@@ -0,0 +1,68 @@
+from util.viz import VizUtil
+from data.data_reader import BizcardDataParser
+from pathlib import Path
+import numpy as np
+import cv2
+import argparse
+
+import random
+from detectron2.data.datasets import register_coco_instances
+from detectron2.data import get_detection_dataset_dicts
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import DatasetCatalog, MetadataCatalog
+
+
+def view_gt_dir(image_dir, json_dir, id_list):
+    with open(id_list) as fp:
+        ids = fp.readlines()
+    for id in ids:
+        # processing hidden files
+        id = Path(id.strip())
+        print(id)
+
+        if not (image_dir / id).with_suffix('.jpg').exists():
+            image_path = str((image_dir / id).with_suffix('.jpeg'))
+        else:
+            image_path = str((image_dir / id).with_suffix('.jpg'))
+
+        key = view_single(str((json_dir / id).with_suffix('.json')), image_path)
+        if key == ord('q'):
+            break
+
+
+def view_single(json, image_path):
+    gt = BizcardDataParser.parse_data(
+        json, image_path)[0].resize_by_ratio(0.15)
+    mask = gt.to_mask()
+    print(mask.shape)
+    view = np.concatenate([VizUtil.viz_boxes(gt.image, gt.words),
+                           VizUtil.viz_mask(mask),
+                           mask], axis=1)
+    cv2.imshow('view', view.astype(np.uint8))
+    key = cv2.waitKey(0)
+    if key == ord('q'):
+        cv2.destroyAllWindows()
+    return key
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--coco', action='store_true')
+    args = parser.parse_args()
+
+    if args.coco:
+        register_coco_instances("bizcard_train", {}, "bizcard_coco_train.json",
+                                "/data/training/business_card/input/source_images")
+        register_coco_instances("bizcard_val", {}, "bizcard_coco_train.json",
+                                "/data/training/business_card/input/source_images")
+        for d in random.sample(get_detection_dataset_dicts(['my_dataset_train']), 3):
+            img = cv2.imread(d["file_name"])
+            visualizer = Visualizer(img[:, :, ::-1], metadata=MetadataCatalog.get('my_dataset_train'), scale=0.2)
+            vis = visualizer.draw_dataset_dict(d)
+            cv2.imshow('', vis.get_image()[:, :, ::-1])
+            cv2.waitKey(0)
+    else:
+        view_gt_dir(
+            Path('/data/training/business_card/input/source_images'),
+            Path('/data/training/business_card/input/ocr_and_ground_truth/OneOCR_GA-0.1.0/Bizcard'),
+            '/data/training/business_card/input/id_lists/20200206/train.txt')
