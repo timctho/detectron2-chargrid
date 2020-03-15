11e45e3a158f9d51bdb36f2b7e68f85f0ece119a
Tim Ho <timho0110@gmail.com>
2020-03-15 16:47:13 +0800

    Refactor inference script

--- projects/CharGrid/inference.py
+++ projects/CharGrid/inference.py
@@ -14,35 +14,84 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
+from data.data_reader import BIZCARD_LABEL_MAP
+import logging
+import glob
 
-if __name__ == '__main__':
+
+def setup(args):
+    logging.basicConfig(level=logging.DEBUG)
     cfg = get_cfg()
-    cfg.merge_from_file('config/mask_rcnn_R50_FPN_3x.yaml')
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-    cfg.freeze()
+    cfg.merge_from_file(args.config_file)
 
+    cfg.DATALOADER.NUM_WORKERS = 8
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
     predictor = DefaultPredictor(cfg)
 
-    from detectron2.utils.visualizer import ColorMode
+    register_coco_instances(
+        "bizcard_train",
+        {},
+        "data/bizcard_coco_train.json",
+        "/data/training/business_card/input/source_images")
+    register_coco_instances(
+        "bizcard_val",
+        {'thing_classes': list(BIZCARD_LABEL_MAP.keys())},
+        "data/bizcard_coco_val.json",
+        "/data/training/business_card/input/source_images")
 
-    register_coco_instances("bizcard_train", {}, "data/bizcard_coco_train.json",
-                            "/data/training/business_card/input/source_images")
-    register_coco_instances("bizcard_val", {}, "data/bizcard_coco_val.json",
-                            "/data/training/business_card/input/source_images")
+    if args.img != '':
+        show_prediction(args.img, predictor)
+    elif args.dir != '':
+        files = []
+        for ext in ['/*.jpg', '/*.png']:
+            files.extend(glob.glob(args.dir + ext))
+        for file in files:
+            key = show_prediction(file, predictor)
+            if key == ord('q'):
+                cv2.destroyAllWindows()
+                break
+    else:
+        dataset_dicts = get_detection_dataset_dicts(['bizcard_val'])
+        for d in random.sample(dataset_dicts, 300):
+            key = show_prediction(d['file_name'], predictor)
+            if key == ord('q'):
+                cv2.destroyAllWindows()
+                break
 
-    dataset_dicts = get_detection_dataset_dicts(['bizcard_val'])
-    for d in random.sample(dataset_dicts, 300):
-        im = cv2.imread(d["file_name"])
-        outputs = predictor(im)
-        v = Visualizer(im[:, :, ::-1],
-                       metadata=MetadataCatalog.get('bizcard_val'),
-                       scale=0.8,
-                       instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-                       )
-        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        cv2.imshow('', cv2.resize((v.get_image()[:, :, ::-1]), None, fx=0.5, fy=0.5))
-        key = cv2.waitKey(0)
-        if key == ord('q'):
-            cv2.destroyAllWindows()
-            break
+
+def show_prediction(img_file, predictor):
+    im = cv2.imread(img_file)
+    outputs = predictor(im)
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=MetadataCatalog.get('bizcard_val'),
+                   scale=0.4,
+                   instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                   )
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow('', v.get_image()[:, :, ::-1])
+    key = cv2.waitKey(0)
+    return key
+
+
+if __name__ == "__main__":
+    parser = default_argument_parser()
+    parser.add_argument('--img', default='', type=str)
+    parser.add_argument('--dir', default='', type=str)
+    args = parser.parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
